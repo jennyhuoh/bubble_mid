f47d8c18fa6ece5302d517c46da773b11e445427
jennyhuoh <huoh.li@gmail.com>
2020-04-29 19:42:24 +0800

    solve problem(modal)

--- src/components/SolveProblem.js
+++ src/components/SolveProblem.js
@@ -1,87 +1,53 @@
-import React, {Component}from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
-import { apisAreAvailable } from 'expo';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {useState}from 'react';
+import { StyleSheet, Text, View, Image, Alert, Modal, TouchableHighlight} from 'react-native';
 import { TouchableOpacity, TextInput } from 'react-native-gesture-handler';
 
-const Stacksolve = createStackNavigator();
-
-function SolveProblem_1 ({navigation : {navigate}}) {
-    return(
-        <View style = {styles.solvebg}>
-            <View style = {styles.robotarea}>
-                <Image style = {styles.chatface} source = {require('../img/img_chatface.png')}/>
-                <Image style = {styles.chatbox} source = {require('../img/img_chatbox.png')}/>
-            </View>
-            <View style = {styles.robotsay1box}><Text style = {styles.robotsay1}>哈囉！最近有哪些事情讓你煩惱呢？</Text></View>   
-            <TouchableOpacity
-                onPress = {() => navigate('SolveProblem_2')}>
-                <View style = {styles.startanswerbg}><Text style = {styles.startanswer}>開始回答</Text></View>
-                
-            </TouchableOpacity>
-        </View> 
-    );
-}
-
-function SolveProblem_2 ({navigation, route}) {
+const SolveProblem = () => {
+    const [modalVisible, setModalVisible] = useState(false);
     return(
         <View>
+            <Modal
+                transparent={true}
+                visible={modalVisible}>
+                <View style = {styles.q1}>
+                    <View style = {styles.q1box}>
+                        <View style = {styles.q1titlebg}><Text style = {styles.q1title}>最近感到煩悶的事情？</Text></View>
+                        <View style = {styles.anssec1}>
+                            <Text style = {styles.num1}>1.</Text>
+                            <TextInput
+                            // onChangeText = {point => setpoint(point)}
+                            // value = {point}
+                                style={{ 
+                                    height: 40,
+                                    width: 200, 
+                                    borderColor: '#378D8F', 
+                                    borderBottomWidth: 1,
+                                }}
+                                placeholder = '輸入事件'
+                                placeholderTextColor = 'gray'
+                                clearTextOnFocus = {true}
+                            />
+                    </View>
+                    <TouchableOpacity
+                        onPress = {() => {setModalVisible(!modalVisible)}}>
+                        <View style = {styles.q1confirmbg}><Text style = {styles.q1confirm}>確認</Text></View>
+                    </TouchableOpacity>    
+                    </View>            
+             </View> 
+            </Modal>
             <View style = {styles.solvebg}>
                 <View style = {styles.robotarea}>
                     <Image style = {styles.chatface} source = {require('../img/img_chatface.png')}/>
                     <Image style = {styles.chatbox} source = {require('../img/img_chatbox.png')}/>
                 </View>
-                </View>
-            <View style = {styles.robotsay1box}><Text style = {styles.robotsay1}>哈囉！最近有哪些事情讓你煩惱呢？</Text></View> 
-            <View style = {styles.q1}>
-                <View style = {styles.q1box}>
-                    <View style = {styles.q1titlebg}><Text style = {styles.q1title}>最近感到煩悶的事情？</Text></View>
-                <View style = {styles.anssec1}>
-                    <Text style = {styles.num1}>1.</Text>
-                    <TextInput
-                          style={{ 
-                              height: 40,
-                              width: 200, 
-                              borderColor: '#378D8F', 
-                              borderBottomWidth: 1,
-                               }}
-                            placeholder = '輸入事件'
-                            placeholderTextColor = 'gray'
-                            clearTextOnFocus = {true}
-                      />
-                </View>
-            <TouchableOpacity
-                onPress = {() => navigation.goBack()}>
-                    <Text>back</Text>
-            </TouchableOpacity>
-                
-                </View>            
-            </View> 
+                <View style = {styles.robotsay1box}><Text style = {styles.robotsay1}>哈囉！最近有哪些事情讓你煩惱呢？</Text></View>   
+                <TouchableOpacity
+                    onPress = {() => {setModalVisible(true)}}>
+                    <View style = {styles.startanswerbg}><Text style = {styles.startanswer}>開始回答</Text></View>
+                </TouchableOpacity>
+            </View>
+       
         </View>
-    );
-}
-
-const SolveProblem = () => {
-    return(
-        <NavigationContainer independent = {true}>
-            <Stacksolve.Navigator initialRouteName = "SolveProblem_1">
-                <Stacksolve.Screen 
-                    name = "SolveProblem_1" 
-                    component = {SolveProblem_1}
-                    options = {() => ({
-                        headerShown: false,
-                    })}
-                
-                      />
-                <Stacksolve.Screen 
-                    name = "SolveProblem_2" 
-                    component = {SolveProblem_2}
-                    options = {() => ({
-                        headerShown: false 
-                      })}/>
-            </Stacksolve.Navigator>
-        </NavigationContainer>
     );
 }
 
@@ -132,7 +98,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0,0,0,0.5)',
         position: 'absolute',
         width: 375,
-        height: 650
+        height: 815
     },
     q1title: {
         color: 'white',
@@ -148,7 +114,7 @@ const styles = StyleSheet.create({
         width: 300,
         height: 420,
         marginLeft: 37.5,
-        marginTop: 130
+        marginTop: 230
     },
     anssec1: {
         flexDirection: 'row',
@@ -157,7 +123,24 @@ const styles = StyleSheet.create({
         marginLeft: 40
     },
     num1: {
-        color: '#378D8F'
+        color: '#378D8F',
+        fontSize: 14
+    },
+    q1confirmbg: {
+        backgroundColor: '#C4E1E7',
+        width: 85,
+        height: 40,
+        borderRadius: 20,
+        marginLeft: 165,
+        marginTop: 170,
+        shadowColor: 'black',
+        shadowOffset: {width: 1},
+        shadowOpacity: 0.2
+    },
+    q1confirm: {
+        color: '#393939',
+        paddingLeft: 29,
+        paddingTop: 12
     }
   });
 
